05ff4b004a217c429f8bd670868410ad174b675f
Tirth Gajjar <11blackspiders@gmail.com>
2018-11-25 10:40:27 +0530

    Removed RTL Files

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,7 @@
 import Pages from "layouts/Pages.jsx";
-import RTL from "layouts/RTL.jsx";
 import Dashboard from "layouts/Dashboard.jsx";
 
 var indexRoutes = [
-  { path: "/rtl", name: "RTL", component: RTL },
   { path: "/pages", name: "Pages", component: Pages },
   { path: "/", name: "Home", component: Dashboard }
 ];
