25c4e0d4d7c37db75dba265143b3fc4a74c460cf
Tirth Gajjar <11blackspiders@gmail.com>
2018-12-28 07:52:20 +0530

    Added support for cloud db

--- server/db/index.js
+++ server/db/index.js
@@ -4,18 +4,18 @@ mongoose.Promise = require('bluebird');
 const { MONGODB_URI } = require('../../setup');
 
 function connect() {
-  mongoose.connect(MONGODB_URI);
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, uri_decode_auth: true });
 
-  mongoose.connection.on("open", function(){
+  mongoose.connection.on("open", function () {
     console.log("MongoDB connection opened");
   });
-  mongoose.connection.on("error", function(){
+  mongoose.connection.on("error", function (a, b) {
     console.log("Error connecting to MongoDB");
   });
-  mongoose.connection.on("disconnected", function(){
+  mongoose.connection.on("disconnected", function () {
     console.log("Disconnected from MongoDB");
   });
-  mongoose.connection.on("reconnected", function(){
+  mongoose.connection.on("reconnected", function () {
     console.log("Re-connected to MongoDB");
   });
 }
--- setup/index.js
+++ setup/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/senzopt',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://admin:%21paragtirth%21@prahalconsultancy-bioqb.mongodb.net/test?retryWrites=true',
     PORT: process.env.PORT || 3000,
 }
