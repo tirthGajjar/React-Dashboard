d373628201fb4efc55ba37431dfa01dcafc0118b
Tirth Gajjar <11blackspiders@gmail.com>
2019-01-05 23:55:50 +0530

    Updated login service

--- src/service/login.js
+++ src/service/login.js
@@ -1,13 +1,14 @@
 import Request from 'axios';
 const baseUrl = "http://localhost:4000";
 
-function makeRequest(method, api = 'login', data) {
+function makeRequest (method, api = 'login', data) {
     return Request[method](baseUrl + api, data)
         .then(r => r);
 };
 
-export const login = function(data){
-    console.log('data:' , data);
+export const login = function (user_name, user_pwd) {
+    console.log('data:', user_name);
+    console.log('data:', user_pwd);
     return new Promise((resolve, reject) => {
         console.log(data);
         makeRequest('post', '/post', data).then((data) => {
@@ -15,6 +16,5 @@ export const login = function(data){
         }).catch((error) => {
             reject(error);
         })
-    })
-
+    });
 };
